1039f3a54468c6edf4f8b87a1f45f6215d7e2f6b
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2017-01-18 23:24:17 -0400

    Add gulp-gh-pages

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
 const gulp = require('gulp');
-const deploy = require('gulp-deploy-git');
+const deploy = require('gulp-gh-pages');
 
 gulp.task('deploy', function() {
-  return gulp.src(['build/**/*', '.gitignore'], { read: false })
-    .pipe(deploy({
-      repository: 'https://github.com/zhevron/gulp-deploy-git.git'
-    }));
+  return gulp.src(['./dist/**/*', '.gitignore'])
+    .pipe(ghPages());
 });
