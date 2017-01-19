9a4bf5599ca8b4d28b4a75c054c96ace0c83d2b2
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2017-01-18 22:59:03 -0400

    Delete all files

--- src/base.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base';
-
-const base = Rebase.createClass({
-  apiKey: "AIzaSyBimPI9qFWCvNQ1FsPPEe4c1UVa-FOp5tY",
-  authDomain: "catch-of-the-day-b2068.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-b2068.firebaseio.com"
-});
-
-export default base;
--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { render } from 'react-dom';
-import './css/style.css';
-
-import Root from './components/Root';
-
-render(<Root/>, document.querySelector("#main"));
-// render(<StorePicker/>, document.querySelector("#main"));
